3dbda1d1064bb7bd6b309b2e9e579fd71ded8a63
curbyourcode <majlessirouz@gmail.com>
2019-01-21 19:48:44 -0800

    fix logo and footer alignment on layout

--- DynamicText.ps1
+++ DynamicText.ps1
@@ -2,24 +2,24 @@ $template = 'layout.html'
 $final_email = 'final_email.html'
 
 $ReportServices =@(
-
 "Angular",
 "React",
 "Ember",
 "Vue",
-"Asp. Net",
+"Asp.Net",
 "Java",
 "Orcale",
-"VS Code",
+"VsCode",
 "Slack")  
 
 $ReportServices =
-@([pscustomobject]@{name="Angular"; color="#b52e31"; footer="b52e31"; code ='2+'; message=''},
+@(
+[pscustomobject]@{name="Angular";color="#b52e31"; code='Angular'; footer="#b52e31";},
 [pscustomobject]@{name="React"; color="#00d8ff"; code='React.Js'; footer="#00d8ff"},
 [pscustomobject]@{name="Ember"; color="#f05e1b"; code='Ember.js'; footer="#f05e1b"},
-[pscustomobject]@{name="Vue"; color=" #41B883"; code='Vue.js'; footer="# #41B883"},
+[pscustomobject]@{name="Vue"; color=" #41B883"; code='Vue.js'; footer="#41B883"},
 [pscustomobject]@{name="Asp.Net"; color="#00008B";code='Asp.Net Core'; footer="#00008B"},
-[pscustomobject]@{name="Java";color="#2b4649 H"; code='Java lanague'; footer="#2b4649 H"},
+[pscustomobject]@{name="Java";color="#2b4649"; code='Java lanague'; footer="#2b4649"},
 [pscustomobject]@{name="Orcale";color="#f80000"; code='Orcale database'; footer="#f80000"},
 [pscustomobject]@{name="VS Code"; color="#0272c6"; code='VS code editor'; footer="#0272c6"},
 [pscustomobject]@{name="Slack";color="#3eb991"; code='Slack'; footer="3eb991"}
@@ -36,7 +36,7 @@ foreach ($service in $ReportServices)
         -replace '\[REPORT_NAME\]',$service.name`
         -replace '\[CODE\]', $service.code`
         -replace '\[FOOTER_COLOR\]', $service.footer`
-        -replace '\[STATUS_MESSAGE\]','Investigating'
+        -replace '\[STATUS_MESSAGE\]','Message'
       
     }  |
     Set-Content ($service.name + ".html")
